dd20347f2cfc1d543124bc6ba09e4790e79caaf0
Your Name <you@example.com>
2023-07-08 22:57:33 +0800

    rpwd

--- python/rpwd/rpwd.py
+++ python/rpwd/rpwd.py
@@ -7,20 +7,15 @@ import sys
 
 
 def gen_random_str(length, ratio=[1,1,1]):
-    ratio_digit = ratio[0] * len(string.digits)
-    ratio_lower = ratio[1] * len(string.ascii_lowercase)
-    ratio_upper = ratio[2] * len(string.ascii_uppercase)
-    gcd = math.gcd(ratio_digit, ratio_lower, ratio_upper)
+    ratio_digit = ratio[0] * 26
+    ratio_lower = ratio[1] * 10
+    ratio_upper = ratio[2] * 10
+    gcd = math.gcd(ratio_digit, math.gcd(ratio_lower, ratio_upper))
     ratio_digit //= gcd
     ratio_lower //= gcd
     ratio_upper //= gcd
-    print('gen_random_str.ratio = ', ratio_digit, ratio_lower, ratio_upper)
-    lcm = math.lcm(ratio_digit, ratio_lower, ratio_upper)
-    mul_digit = lcm // ratio_digit
-    mul_lower = lcm // ratio_lower
-    mul_upper = lcm // ratio_upper
-    print('gen_random_str.mul = ', mul_digit, mul_lower, mul_upper)
-    src_str = string.digits * mul_digit + string.ascii_lowercase * mul_lower + string.ascii_uppercase * mul_upper
+    print('gen_random_str.ratio = ', len(string.digits) * ratio_digit, len(string.ascii_lowercase) * ratio_lower, len(string.ascii_uppercase) * ratio_upper)
+    src_str = string.digits * ratio_digit + string.ascii_lowercase * ratio_lower + string.ascii_uppercase * ratio_upper
     return ''.join([random.choice(src_str) for i in range(length)])
 
 
@@ -31,7 +26,12 @@ def main():
         print('[CMD] length')
         exit(-1)
     length = int(sys.argv[1])
-    rand_str = gen_random_str(length, [3, 4, 4])
+
+    ratio = [2, 3, 3]
+
+    print(ratio)
+    rand_str = gen_random_str(length, ratio)
+    print()
     print(rand_str)
 
 
